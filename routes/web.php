bef9b09930bba127885643eed8aa34eda873de6c
Wan Aida <wnaida05@gmail.com>
2023-11-15 00:04:27 +0800

    Student Controller,Model,Seeder,Factory
    Student Controller,Model,Seeder,Factory

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('liststudent', function () {
+    return view('student.liststudent');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
